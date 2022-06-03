767f5e7741256fa88f5ad8e66d4221b357e2d6cd
Marcosmv124 <105815107+Marcosmv124@users.noreply.github.com>
2022-06-03 10:08:13 -0700

    Tarea-Archivos-Escritura

--- Archivos_EscrituraTarea/Program.cs
+++ Archivos_EscrituraTarea/Program.cs
@@ -9,26 +9,45 @@ namespace Archivos_EscrituraTarea
 {
     class Program
     {
+        class Persona
+        {
+            // Campos de la clase 
+            public string Nombre;
+            public int Edad; 
+
+         //Constructor de la clase
+          public Persona(string Nombre, int Edad)
+          {
+                this.Nombre = Nombre; 
+                this.Edad = Edad; 
+          }
+            // Destructor de la clase persona
+            ~Persona()
+            {
+                Console.WriteLine("Memoria liberada de la clase persona: "); 
+            }
+
+        }
         static void Main(string[] args)
         {
             StreamWriter sw = new StreamWriter("ejemplo.txt",true);// Cambios
             // Si el archivo no existe lo creara
             // Si el archivo existe, escribira en el
 
-            string[] Lines =
-            {
-                "Esto es la primera Linea",
-                "Esto es la segunda Linea",
-                "Esto es la tercera Linea",
-                "Esto es la cuarta Linea"
-            };
+          // Declaración de variables
+            string Nom;
+            int Edad; 
+ 
+                Console.Write("\nIngresa el nombre: "); 
+                Nom = Console.ReadLine();
+                Console.Write("\nIngresa Edad : ");
+                Edad = int.Parse(Console.ReadLine());
+                Persona Datos = new Persona(Nom, Edad);
 
-            foreach (string Line in Lines)
-            {
-                sw.WriteLine(Line);
-            }
+            sw.WriteLine("\n" + Datos.Nombre + " " + "\n" +
+               Datos.Edad ); 
+
             sw.Close();
-
             Console.WriteLine("Escribiendo........");
             Console.ReadLine();
         }
